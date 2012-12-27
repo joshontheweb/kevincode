267469238bb363a8533dcdbd9a132f522708c9bb
unknown <kevin.dolan@DALL-KDOLAN.milliman.com>
2012-12-27 01:29:40 -0700

    stuff

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ var name = prompt('what\'s your name?');
 var $p1El = $('.p1Name');
 $p1El.text(name);
 console.log('kevin');
+
+var socket = io.connect('http://localhost');
+  socket.on('news', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+  });
